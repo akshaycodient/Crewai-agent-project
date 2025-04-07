c1f92ad9fd1607cc897432642bd1e1bda0c6204d
akshaycodient <akshaycodient@gmail.com>
2025-04-07 21:29:45 +0530

    vue-dashboard added

---
+++ vue-dashboard/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [vue()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    }
+  }
+})
